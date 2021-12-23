46bc8a51abceebd391f7f894f5f59b200f6cb50b
Kido1111 <96579430+Kido1111@users.noreply.github.com>
2021-12-23 19:49:26 +0700

    Create Newfile.py

---
+++ Newfile.py
@@ -0,0 +1,21 @@
+import os,sys, time,requests
+
+os.system("clear")
+print("
+         Termux sms & VDIX2Z YT
+")
+hee = input("เบอร์ : ")
+hum = in(input ("จำนวน : "))
+
+heder = {"
+"}
+
+data = {" "}
+
+for i in range(hum):
+	rip = requests.post(' ',headers=heder,json=data)
+	if'eror'in rip.text:
+		print("กำลังยิง")
+	else:
+		print("ยิงไร")
+		
